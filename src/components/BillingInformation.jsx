8642a2fdfd99704889844a7b0a08ddca962e1846
Akash <akashaanil32@gmail.com>
2023-11-13 17:28:34 +0530

    #commit

--- src/components/BillingInformation.jsx
+++ src/components/BillingInformation.jsx
@@ -1,10 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import { Typography, Button, Input } from "@mui/material";
+import { Typography, Button, Input, TextField } from "@mui/material";
 import SimpleSnackbar from "./SimpleSnackbar";
 
-const BillingInformation = ( amount ) => {
+const BillingInformation = (amount) => {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    address: "",
+  });
+
+  const [validation, setValidation] = useState(false);
+
+  console.log(userInfo, "userinfo");
 
   const loadScript = (src) => {
     return new Promise((resolve) => {
@@ -23,25 +31,27 @@ const BillingInformation = ( amount ) => {
   };
 
   const razorPayinit = async (amount) => {
-    const res = await loadScript("https://checkout.razorpay.com/v1/checkout.js");
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
     if (!res) {
       alert("You are offline, failed to load...");
       return;
     }
 
     const options = {
-      key: 'rzp_test_Q3uKNb9YbqSLpf',
-      currency: 'INR',
+      key: "rzp_test_Q3uKNb9YbqSLpf",
+      currency: "INR",
       amount: Object.values(amount)[0] * 100,
-      name: 'foodtopia payment',
-      description: 'Thanks for your order',
+      name: "foodtopia payment",
+      description: "Thanks for your order",
       handler: function (response) {
         alert(response.razorpay_payment_id);
-        alert('Payment successfully');
+        alert("Payment successfully");
         showSnackBar(true);
       },
       prefill: {
-        name: 'foodtopia ',
+        name: "foodtopia ",
       },
     };
 
@@ -56,37 +66,73 @@ const BillingInformation = ( amount ) => {
   };
 
   const handleCloseSnackbar = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setSnackbarOpen(false);
   };
 
-  const totalAmount = amount  ? Object.values(amount) : 0;
+  const userData = (e) => {
+    const nameRegex = /^[a-zA-Z\s]*$/;
+    const isValidName = nameRegex.test(e.target.value);
+
+    if (!isValidName) {
+      setValidation(true);
+    } else {
+      setValidation(false);
+      setUserInfo({
+        ...userInfo,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const totalAmount = amount ? Object.values(amount) : 0;
 
   return (
     <div>
       <Typography variant="h5" gutterBottom>
         Billing Information
       </Typography>
+      <div className="flex flex-col gap-2">
       <div>
-        <Input fullWidth color="primary" type="text" placeholder="Name" />
+        <TextField
+          label={"name"}
+          fullWidth
+          color="primary"
+          type="text"
+          name="name"
+          onChange={userData}
+          helperText={validation ? "please enter a value" : ""}
+          error={validation ? "error" : null}
+        />
       </div>
       <div>
-        <Input fullWidth type="text" placeholder="Address" />
+        <TextField
+          label={"addres"}
+          fullWidth
+          type="text"
+          name="address"
+          onChange={userData}
+          helperText={validation ? "please enter a value" : ""}
+          error={validation ? "error" : null}
+        />
+      </div>
       </div>
       <div>
         <Typography gutterBottom>Total : {totalAmount}</Typography>
       </div>
-      <Button
-        onClick={() => {
-          razorPayinit(totalAmount);
-        }}
-        variant="contained"
-        color="primary"
-      >
-        Place Order
-      </Button>
+      {userData && (
+        <Button
+          onClick={() => {
+            if (!validation && totalAmount) razorPayinit(totalAmount);
+          }}
+          variant="contained"
+          color="primary"
+        >
+          Place Order
+        </Button>
+      )}
 
       <SimpleSnackbar
         open={snackbarOpen}
