9da0ca2d08b684700f91e02704df1e3bdd3da142
Akash <akashaanil32@gmail.com>
2023-12-14 17:13:52 +0530

    #backendIntegration_emailSend updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-
+// App.js
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./components/ThemeContext";
+import Signup from "./components/Signup";
 import Categories from "./components/Categories";
 import Delivery from "./components/Delivery";
 import Featured from "./components/Featured";
@@ -7,23 +12,27 @@ import Meals from "./components/Meals";
 import NewsLetter from "./components/NewsLetter";
 import TopNav from "./components/TopNav";
 import TopPicks from "./components/TopPicks";
-import { ThemeProvider} from "./components/ThemeContext";
 
 function App() {
-
   return (
-    <ThemeProvider>
-    <div className="App">
-      <TopNav />
-      <Featured/>
-      <Delivery/>
-      <TopPicks/>
-      <Meals/>
-      <Categories/>
-      <NewsLetter/>
-    <Footer/>
-    </div>
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <div className="App">
+          <TopNav />
+          <Featured />
+          <Delivery />
+          <TopPicks />
+          <Meals />
+          <Categories />
+          <NewsLetter />
+          <Footer />
+          <Routes>
+          {/* <Route path="/signup" element={<Signup />} /> */}
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -1,26 +1,21 @@
-// eslint-disable-next-line no-unused-vars
-import React, { createContext, useContext, useState, useEffect,useReducer } from "react";
+// ThemeContext.jsx
+import React, { createContext, useContext, useState, useEffect, useReducer } from "react";
 import Categories from "./Categories";
 import Delivery from "./Delivery";
 import Featured from "./Featured";
 import Footer from "./Footer";
 import Meals from "./Meals";
 import NewsLetter from "./NewsLetter";
-import TopNav from "./TopNav";
 import TopPicks from "./TopPicks";
 import { operationContext, operationReducer } from "./operationReducer";
-
+import TopNav from "./TopNav"; // Include TopNav here if needed
 
 const ThemeContext = createContext();
 
-
-
-export function ThemeProvider() {
+export function ThemeProvider({ children }) {
   const savedThemeMode = localStorage.getItem("themeMode");
   const [isDarkTheme, setIsDarkTheme] = useState(savedThemeMode === "dark");
-  const [themeMode, setThemeMode] = useState(
-    savedThemeMode || "light"
-  );
+  const [themeMode, setThemeMode] = useState(savedThemeMode || "light");
 
   const toggleTheme = () => {
     setIsDarkTheme((prev) => !prev);
@@ -32,24 +27,14 @@ export function ThemeProvider() {
   }, [isDarkTheme]);
 
   return (
-    <>
     <ThemeContext.Provider value={{ isDarkTheme, toggleTheme, themeMode }}>
-    <operationContext.Provider value={useReducer(operationReducer, { isDialogOpen: false })}>
-      <TopNav />
-      <Featured />
-      <Delivery />
-      <TopPicks />
-      <Meals />
-      <Categories />
-      <NewsLetter />
-      <Footer />
-    </operationContext.Provider>
+      <operationContext.Provider value={useReducer(operationReducer, { isDialogOpen: false })}>
+        {children}
+      </operationContext.Provider>
     </ThemeContext.Provider>
-    </>
   );
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
 export function useTheme() {
   return useContext(ThemeContext);
 }
